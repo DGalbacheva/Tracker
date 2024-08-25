51ff9344bbf144506b371391560e145690133699
DoroteyaGalbacheva <dglbacheva@gmail.com>
2024-08-25 11:16:59 +0300

    Schedule added

---
+++ Tracker/ScheduleDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ScheduleDelegate.swift
+//  Tracker
+//
+//  Created by Doroteya Galbacheva on 25.08.2024.
+//
+
+import Foundation
---
+++ Tracker/SectionHeader.swift
@@ -0,0 +1,8 @@
+//
+//  SectionHeader.swift
+//  Tracker
+//
+//  Created by Doroteya Galbacheva on 25.08.2024.
+//
+
+import Foundation
