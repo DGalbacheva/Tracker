48dba77653be79edea5933d01be7727a2c1bbc10
DoroteyaGalbacheva <dglbacheva@gmail.com>
2024-08-25 11:17:11 +0300

    Schedule added

--- Tracker/ScheduleDelegate.swift
+++ Tracker/ScheduleDelegate.swift
@@ -2,7 +2,11 @@
 //  ScheduleDelegate.swift
 //  Tracker
 //
-//  Created by Doroteya Galbacheva on 25.08.2024.
+//  Created by Doroteya Galbacheva on 23.08.2024.
 //
 
 import Foundation
+
+protocol ScheduleDelegate: AnyObject {
+    func weekDaysChanged(weedDays: [WeekDay])
+}
--- Tracker/SectionHeader.swift
+++ Tracker/SectionHeader.swift
@@ -2,7 +2,28 @@
 //  SectionHeader.swift
 //  Tracker
 //
-//  Created by Doroteya Galbacheva on 25.08.2024.
+//  Created by Doroteya Galbacheva on 23.08.2024.
 //
 
-import Foundation
+import UIKit
+
+//Create a class for the section header
+final class SectionHeader: UICollectionReusableView {
+    var titleLabel: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        titleLabel = UILabel()
+        titleLabel.font = UIFont.systemFont(ofSize: 19, weight: .bold)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 28),
+            titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
