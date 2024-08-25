af534f1beb673646acdf9494724a2c9d35eb5cff
DoroteyaGalbacheva <dglbacheva@gmail.com>
2024-08-25 11:07:37 +0300

    Interface added

---
+++ Tracker/NewHabitViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NewHabitViewController.swift
+//  Tracker
+//
+//  Created by Doroteya Galbacheva on 25.08.2024.
+//
+
+import Foundation
---
+++ Tracker/StatsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  StatsViewController.swift
+//  Tracker
+//
+//  Created by Doroteya Galbacheva on 25.08.2024.
+//
+
+import Foundation
