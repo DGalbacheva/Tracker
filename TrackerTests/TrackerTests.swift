b2d50082075ea948eaf6f7cbcb32ff9a7ee7aa8f
DGalbacheva <dglbacheva@gmail.com>
2024-11-23 15:14:23 +0300

    Added tests for light theme.

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -6,31 +6,27 @@
 //
 
 import XCTest
+import SnapshotTesting
 @testable import Tracker
 
 final class TrackerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTrackerViewControllerForEmptyState() {
+        let testVC = TrackersViewController()
+        let coreDataManager = CoreDataManager.shared
+        coreDataManager.trackerCategoryStore.removeAllTrackerCategory()
+        coreDataManager.trackerStore.removeAllTrackers()
+        coreDataManager.trackerRecordStore.removeAllTrackerRecords()
+        assertSnapshot(of: testVC, as: .image)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testTrackerViewControllerForNormalState() {
+        let testVC = TrackersViewController()
+        let coreDataManager = CoreDataManager.shared
+        coreDataManager.trackerCategoryStore.removeAllTrackerCategory()
+        coreDataManager.trackerStore.removeAllTrackers()
+        coreDataManager.trackerRecordStore.removeAllTrackerRecords()
+        coreDataManager.trackerStore.addNewTracker(tracker: Tracker(id: UUID(), title: "Reading", color: .colorSet1, emoji: "🙌", schedule: [.monday, .tuesday, .wednesday, .thursday, .friday, .saturday, .sunday]), categoryName: "Book")
+        assertSnapshot(of: testVC, as: .image)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
