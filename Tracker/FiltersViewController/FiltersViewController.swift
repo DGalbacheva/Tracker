719714e7089a60ae7dc11bd09c183817186bca38
DGalbacheva <dglbacheva@gmail.com>
2024-11-26 16:13:33 +0300

    Filter search functionality has been implemented, as well as sorting functionality.

---
+++ Tracker/FiltersViewController/FiltersViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FiltersViewController.swift
+//  Tracker
+//
+//  Created by MacBook Pro 15 on 26.11.2024.
+//
+
+import Foundation
---
+++ Tracker/Helpers/FiltersCases.swift
@@ -0,0 +1,8 @@
+//
+//  FiltersCases.swift
+//  Tracker
+//
+//  Created by MacBook Pro 15 on 26.11.2024.
+//
+
+import Foundation
